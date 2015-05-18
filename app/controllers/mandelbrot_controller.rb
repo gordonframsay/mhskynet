c2c81855304bf7a4a3a1f6c9e1f595395779c80b
Gordon F Ramsay <gordonframsay@gmail.com>
2015-05-17 22:55:58 -0700

    Latest Mandelbrot.

--- app/controllers/mandelbrot_controller.rb
+++ app/controllers/mandelbrot_controller.rb
@@ -1,11 +1,11 @@
 class MandelbrotController < ApplicationController
 
  def index
-  @x = -0.746
-  @y = 0.21065
-  @zoom = 200000
-  @max_iter = 120
-  @antialias = 1.0
+  @x = (params[:center_x])?(params[:center_x].to_d):-0.746
+  @y = (params[:center_y])?(params[:center_y].to_d):0.21065
+  @zoom = (params[:zoom])?(params[:zoom].to_i):200000
+  @max_iter = (params[:max_iter])?(params[:max_iter].to_i):120
+  @antialias = (params[:antialias])?(params[:antialias].to_d):1.0
  end
 
 end
