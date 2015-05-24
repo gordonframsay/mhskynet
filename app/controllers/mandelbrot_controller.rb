446dc63ce8a13cc49c2b4b256a958e2fb75478d9
Gordon F Ramsay <gordonframsay@gmail.com>
2015-05-23 18:23:29 -0700

    Tweak to antialiasing default.

--- app/controllers/mandelbrot_controller.rb
+++ app/controllers/mandelbrot_controller.rb
@@ -5,7 +5,7 @@ class MandelbrotController < ApplicationController
   @y = (params[:center_y])?(params[:center_y].to_d):0.21065
   @zoom = (params[:zoom])?(params[:zoom].to_i):200000
   @max_iter = (params[:max_iter])?(params[:max_iter].to_i):120
-  @antialias = (params[:antialias])?(params[:antialias].to_d):1.0
+  @antialias = (params[:antialias])?(params[:antialias].to_d):1
  end
 
 end
