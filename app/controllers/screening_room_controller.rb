bf9c3fd35629443d47288793d8d507d1a2c55bd7
Gordon F Ramsay <gordonframsay@gmail.com>
2015-07-03 10:29:59 -0700

    Wizard of Meh test video.

--- app/controllers/screening_room_controller.rb
+++ app/controllers/screening_room_controller.rb
@@ -1,10 +1,10 @@
 class ScreeningRoomController < ApplicationController
 
  def index
-  @movie_title = "The Three Stooges Best Episodes Ever"
-  @movie_time = Time.gm(2015,07,03,15,30)
-  @youtube_video_id = "YLEMvJZ-X1c"
-  @movie_length = 7018
+  @movie_title = "Pogo - Wizard of Meh - 10 hours"
+  @movie_time = Time.gm(2015,07,03,17,30)
+  @youtube_video_id = "3-B2cuFH6oc"
+  @movie_length = 10 * 60 * 60
   @meta_refresh_times = []
 #  @meta_refresh_times << (@movie_time - Time.now).round if (@movie_time > Time.now)
 #  @meta_refresh_times << (@movie_time - Time.now + @movie_length).round if ((@movie_time + @movie_length) < Time.now)
