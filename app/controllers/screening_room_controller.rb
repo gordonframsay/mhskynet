83d24470a8aad1dcb627491757f2eac797f52ffe
Gordon F Ramsay <gordonframsay@gmail.com>
2015-07-02 21:50:08 -0700

    Refresh at movie start.

--- app/controllers/screening_room_controller.rb
+++ app/controllers/screening_room_controller.rb
@@ -4,6 +4,10 @@ class ScreeningRoomController < ApplicationController
   @movie_title = "Plan Nine from Outer Space (1959)"
   @movie_time = Time.gm(2015,07,03,05,0)
   @movie_url = "https://youtu.be/cc-zpN2hQno"
+  @movie_length = 4701
+  @meta_refresh_times = []
+  @meta_refresh_times << (@movie_time - Time.now).round if (@movie_time > Time.now)
+#  @meta_refresh_times << (@movie_time - Time.now + @movie_length).round if ((@movie_time + @movie_length) < Time.now)
  end
 
 end
