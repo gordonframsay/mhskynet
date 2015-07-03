929bf8f1613ddc66845fcf6791b5403d7d1edb33
Gordon F Ramsay <gordonframsay@gmail.com>
2015-07-03 08:38:34 -0700

    Testing some new Youtube API stuff.

--- app/controllers/screening_room_controller.rb
+++ app/controllers/screening_room_controller.rb
@@ -1,12 +1,12 @@
 class ScreeningRoomController < ApplicationController
 
  def index
-  @movie_title = "Plan Nine from Outer Space (1959)"
-  @movie_time = Time.gm(2015,07,03,05,0)
-  @movie_url = "https://youtu.be/cc-zpN2hQno"
-  @movie_length = 4701
+  @movie_title = "The Three Stooges Best Episodes Ever"
+  @movie_time = Time.gm(2015,07,03,15,30)
+  @youtube_video_id = "YLEMvJZ-X1c"
+  @movie_length = 7018
   @meta_refresh_times = []
-  @meta_refresh_times << (@movie_time - Time.now).round if (@movie_time > Time.now)
+#  @meta_refresh_times << (@movie_time - Time.now).round if (@movie_time > Time.now)
 #  @meta_refresh_times << (@movie_time - Time.now + @movie_length).round if ((@movie_time + @movie_length) < Time.now)
  end
 
