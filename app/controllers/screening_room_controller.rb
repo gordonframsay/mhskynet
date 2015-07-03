cc7ef510dc99db77e2141f542612d14d96448f74
Gordon F Ramsay <gordonframsay@gmail.com>
2015-07-02 21:17:30 -0700

    New movie.

--- app/controllers/screening_room_controller.rb
+++ app/controllers/screening_room_controller.rb
@@ -1,9 +1,9 @@
 class ScreeningRoomController < ApplicationController
 
  def index
-  @movie_title = "Coming soon..."
+  @movie_title = "Plan Nine from Outer Space (1959)"
   @movie_time = Time.gm(2015,07,03,05,0)
-  @movie_url = "https://youtu.be/PhxE1RFtAWY"
+  @movie_url = "https://youtu.be/cc-zpN2hQno"
  end
 
 end
