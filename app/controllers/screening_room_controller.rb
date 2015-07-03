8a5e5b0653b4075fc273efca397e6065f24c1dea
Gordon F Ramsay <gordonframsay@gmail.com>
2015-07-02 20:34:35 -0700

    Off the air...

--- app/controllers/screening_room_controller.rb
+++ app/controllers/screening_room_controller.rb
@@ -1,9 +1,9 @@
 class ScreeningRoomController < ApplicationController
 
  def index
-  @movie_title = "1958 The Brain Eaters"
-  @movie_time = Time.gm(2015,07,03,02,30)
-  @movie_url = "https://youtu.be/hOrP34Q-1BA"
+  @movie_title = "Coming soon..."
+  @movie_time = Time.gm(2015,07,03,05,0)
+  @movie_url = "https://youtu.be/PhxE1RFtAWY"
  end
 
 end
