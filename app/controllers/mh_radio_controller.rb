43bc1ddf5729da812788aaa841d3f497e1bdee9f
Gordon F Ramsay <gordonframsay@gmail.com>
2015-07-31 14:17:44 -0700

    Longer session expiration, radio playlist.

--- app/controllers/mh_radio_controller.rb
+++ app/controllers/mh_radio_controller.rb
@@ -1,12 +1,19 @@
 class MhRadioController < ApplicationController
 
+ # Temp test page
  def google_test
   @google_domain_api_key = get_config("google_api_key")
  end
 
+ # For testing with Google APIs
+ def oauth2callback
+  logger.warn(params.inspect)
+  render :text => "nothing"
+ end
+
  def index
   @page_title = "▶︎ " + @page_title+ " - Radio"
-  if request.post?
+  if (request.post? && params[:identifier])
    @song = RadioSong.new
    @song.identifier = params[:identifier]
    @song.source_ip = request.remote_ip
@@ -18,13 +25,31 @@ class MhRadioController < ApplicationController
     redirect_to :action => :index
    end
   else
-   if session[:last_song_played]
-    @song = RadioSong.select("random(),*").where(["id != ?", session[:last_song_played]]).order("random").first # TODO: let's later make this a bit more intelligent.. shuffle instead of random
+   # Creates a random shuffle every hour
+   @songs = RadioSong.select("md5(identifier || '"+Time.now.strftime("%a %b %e %H %Y")+"'),*").order("md5")
+   if cookies[:mh_radio_playlist]
+    @play_list = JSON.parse(cookies[:mh_radio_playlist])
+    last_updated = cookies[:mh_radio_playlist_last_updated].to_time
+    @play_list += RadioSong.where(["created_at > ?", last_updated]).map {|x| x.id }
+    @play_list.uniq!
    else
-    @song = RadioSong.select("random(),*").order("random").first # TODO: let's later make this a bit more intelligent.. shuffle instead of random
+    @play_list = @songs.map {|x| x.id }
    end
+   if params[:do_not_play]
+    @play_list.reject! {|x| x == params[:current_song].to_i }
+    cookies.permanent[:mh_radio_playlist] = JSON.generate(@play_list)
+    cookies.permanent[:mh_radio_playlist_last_updated] = Time.now.gmtime.to_s
+   end
+   @songs = @songs.reject {|x| ! @play_list.include?(x.id) } # TODO: not sure why reject! doesn't work here... grumble
+   if session[:last_song_played_index]
+    session[:last_song_played_index] += 1
+    @song = @songs[session[:last_song_played_index] % @songs.length]
+   else
+    session[:last_song_played_index] = 0
+    @song = @songs.first
+   end
+   flash[:notice] = "Playing index: "+session[:last_song_played_index].to_s if (Rails.env.to_s == "development")
   end
-  session[:last_song_played] = @song.id
  end
 
  def submitted_songs
@@ -32,6 +57,23 @@ class MhRadioController < ApplicationController
   @page_title =  @page_title + " - Submitted Songs"
   @user_session_id = session.id
   @songs = RadioSong.order("id desc")
+  if cookies[:mh_radio_playlist]
+   @play_list = JSON.parse(cookies[:mh_radio_playlist])
+   last_updated = cookies[:mh_radio_playlist_last_updated].to_time
+   @play_list += RadioSong.where(["created_at > ?", last_updated]).map {|x| x.id }
+   @play_list.uniq!
+  else
+   @play_list = @songs.map {|x| x.id }
+  end
+  if request.post?
+   if (params[:play_list])
+    @play_list = params[:play_list].keys.map {|x| x.to_i }
+   else
+    @play_list = @songs.map {|x| x.id }
+   end
+   cookies.permanent[:mh_radio_playlist] = JSON.generate(@play_list)
+   cookies.permanent[:mh_radio_playlist_last_updated] = Time.now.gmtime.to_s
+  end
  end
 
  def delete
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,7 +1,7 @@
 class Session < ActiveRecord::Base
 
   def self.clean_up
-   destroy_all(["updated_at < ?", Time.now - 60 * 60 * 24 * 4])
+   destroy_all(["updated_at < ?", Time.now.gmtime - 60 * 60 * 24 * 14])
   end
 
 end
