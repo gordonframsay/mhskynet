939ecf9043441f69ba91e97b36f2cb7e60a31e43
Gordon F Ramsay <gordonframsay@gmail.com>
2016-06-01 01:05:45 -0700

    Test CDN proxy.

--- app/controllers/dark_room_controller.rb
+++ app/controllers/dark_room_controller.rb
@@ -4,4 +4,11 @@ class DarkRoomController < ApplicationController
   @page_title =  @page_title + " - Dark Room"
  end
 
+ def mirror
+  if params[:id]
+   c = Curl::Easy.perform(params[:id])
+   send_data c.body, :type => c.content_type, :disposition => 'inline'
+  end
+ end
+
 end
