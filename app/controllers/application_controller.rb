982a50b8f1507fef7a53b26484eda73ef4f1102d
Philip Edelbrock <philipedelbrock@Philips-MacBook-Pro.local>
2018-05-20 13:00:34 -0700

    Update KA live stream.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,7 +51,7 @@ class ApplicationController < ActionController::Base
    else
     @live_event = true
     @movie_title = "Kitten Academy Live Stream"
-    @movie_identifier = "vtI_xgeZVwg"
+    @movie_identifier = "68deEAd37Zs"
     @movie_length = 100000000
     @movie_time = Time.gm(2017,6,20,0)
     @movie_service = "youtube"
